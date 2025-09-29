8e3f32182fa7da536f291302a968f14f1b7413d8
Talha Tahir <140109354+Talha-Tahir2001@users.noreply.github.com>
2025-09-30 00:18:06 +0500

    feat: define routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,40 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  // Define your routes here
+  {
+    path: '',
+    loadComponent: () => import('./pages/home/home').then((m) => m.Home),
+  },
+  {
+    path: 'tools',
+    loadComponent: () => import('./pages/tools/tools').then((m) => m.Tools),
+  },
+  {
+    path: 'tools/marketing',
+    loadComponent: () =>
+      import('./pages/tools/marketing/marketing').then((m) => m.Marketing),
+  }, 
+  {
+    path: 'tools/persona',
+    loadComponent: () =>
+      import('./pages/tools/persona/persona').then((m) => m.Persona),
+  },
+  {
+    path: 'tools/moodboard',
+    loadComponent: () =>
+      import('./pages/tools/moodboard/moodboard').then((m) => m.Moodboard),
+  }, 
+  {
+    path: 'about',
+    loadComponent: () => import('./pages/about/about').then((m) => m.About),
+  },
+  {
+    path: 'pricing',
+    loadComponent: () => import('./pages/pricing/pricing').then((m) => m.Pricing),
+  },
+  {
+    path: 'contact',
+    loadComponent: () => import('./pages/contact/contact').then((m) => m.Contact),
+  },
+];
