f7ad2383495711db153e9ce696cbfd5f1ddb65f3
Talha Tahir <140109354+Talha-Tahir2001@users.noreply.github.com>
2025-09-30 00:16:23 +0500

    refactor (ui): add header, footer
    - implement header and footer

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,16 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { ButtonModule } from 'primeng/button';
+import { providePrimeNG } from 'primeng/config';
+import { Header } from "./components/header/header";
+import { Footer } from "./components/footer/footer";
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ButtonModule, Header, Footer],
   templateUrl: './app.html',
   styles: [],
 })
 export class App {
+ 
   protected readonly title = signal('Inceptia');
 }
