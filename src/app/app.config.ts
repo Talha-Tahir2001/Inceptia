cccc03e5b41eeafbd12bcf4c06f3e97d993e47b9
Talha Tahir <140109354+Talha-Tahir2001@users.noreply.github.com>
2025-09-30 00:15:01 +0500

    refactor: change config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,45 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  inject,
+  provideAppInitializer,
+  provideBrowserGlobalErrorListeners,
+  provideZonelessChangeDetection,
+} from '@angular/core';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import {
+  provideClientHydration,
+  withEventReplay,
+} from '@angular/platform-browser';
+
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeuix/themes/aura';
+import { StylePreset } from '../style-preset';
+import { ThemeService } from './services/theme-service';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    ThemeService,
+    provideAppInitializer(() => {
+      const themeService = inject(ThemeService).init();
+    }),
+    provideAnimationsAsync(),
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes), provideClientHydration(withEventReplay())
-  ]
+    provideRouter(routes),
+    provideClientHydration(withEventReplay()),
+    providePrimeNG({
+      theme: {
+        preset: StylePreset,
+        options: {
+          // prefix: 'p',
+          darkModeSelector: '.my-app-dark',
+          cssLayer: false,
+        },
+      },
+    }),
+  ],
 };
