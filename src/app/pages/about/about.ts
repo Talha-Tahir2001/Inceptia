6699d9cca87fa6c507258d0fda4d1c453a39c6bb
Talha Tahir <140109354+Talha-Tahir2001@users.noreply.github.com>
2025-09-30 00:31:27 +0500

    feat (ui): add a about page

---
+++ src/app/pages/about/about.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+
+@Component({
+  selector: 'app-about',
+  imports: [CardModule, ButtonModule],
+  templateUrl: './about.html',
+  styles: ``
+})
+export class About {
+
+}
